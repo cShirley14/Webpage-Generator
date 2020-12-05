ce5be33a690a287bfc8ca57fc1119e930d860d8b
Chantal Shirley <chantal.c.shirley.14@Dartmouth.edu>
2020-12-05 16:18:43 -0600

    Added more code and changed messages resource bundle content

--- src/main/java/com/shirley/left_form/FormPanel.java
+++ src/main/java/com/shirley/left_form/FormPanel.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.shirley.left_form;
 
+import com.shirley.Languages;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ResourceBundle;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 /**
  *
@@ -24,7 +26,57 @@ public class FormPanel extends JPanel {
     private JTextField red_field;
     private JTextField blue_field;
     private JTextField green_field;
+    private JTextField content_field;
     private JButton submit_button;
     private FormListener formListener;
+    private ResourceBundle messages = Languages.getResourceBundle();
+    
+    public FormPanel() {
+        Dimension size = getPreferredSize();
+        size.width = 250;
+        setPreferredSize(size);
+        
+        Border inner_border = BorderFactory.createTitledBorder(
+                messages.getString("title"));
+        Border outer_border = BorderFactory.createEmptyBorder(5,5,5,5);
+        setBorder(BorderFactory.createCompoundBorder(outer_border, 
+                inner_border));
+        
+        title_label = new JLabel(messages.getString("request-title"), 
+                JLabel.LEFT);
+        red_label = new JLabel(messages.getString("request-red"),
+                JLabel.LEFT);
+        blue_label = new JLabel(messages.getString("request-blue"),
+                JLabel.LEFT);
+        green_label = new JLabel(messages.getString("request-green"),
+                JLabel.LEFT);
+        content_label = new JLabel(messages.getString("request-content"),
+                JLabel.LEFT);
+        submit_button = new JButton(messages.getString("submit-button"));
+        
+        title_field = new JTextField(10);
+        red_field = new JTextField(10);
+        blue_field = new JTextField(10);
+        green_field = new JTextField(10);
+        content_field = new JTextField(10);
+        
+        submit_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String title = title_field.getText();
+                String red = red_field.getText();
+                String blue = blue_field.getText();
+                String green = green_field.getText();
+                String content = content_field.getText();
+                
+                FormEvent event = new FormEvent(this, title, red, blue,
+                    green, content);
+                if (formListener != null) {
+                    formListener.formSubmit(event);
+                }
+            }
+        });
+        
+    }
     
 }
