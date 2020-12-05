34c8b2d38b76d05ae57841dd33bbc82b82c9e1f4
Chantal Shirley <chantal.c.shirley.14@Dartmouth.edu>
2020-12-05 00:33:38 -0600

    Added form panel code

---
+++ src/main/java/com/shirley/TextPanel.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shirley;
+
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author Chantal Shirley
+ */
+public class TextPanel extends JPanel {
+    private JTextArea textArea;
+    
+    public TextPanel() {
+        textArea = new JTextArea();
+        setLayout(new BorderLayout());
+        add(new JScrollPane(textArea), BorderLayout.CENTER);
+    }
+    
+    public void appendText(String text) {
+        textArea.append(text);
+    }
+    
+    public String getText() {
+        return textArea.getText();
+    }
+}
---
+++ src/main/java/com/shirley/left_form/FormPanel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shirley.left_form;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Chantal Shirley
+ */
+public class FormPanel extends JPanel {
+    private JLabel title_label;
+    private JLabel red_label;
+    private JLabel green_label;
+    private JLabel blue_label;
+    private JLabel content_label;
+}
